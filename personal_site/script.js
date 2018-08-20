e2f7cf9a98d35d256b9dcf29e2cf158fdd1807c8
jeanpaulc <40746319+jeanpaulc@users.noreply.github.com>
2018-08-19 19:45:37 -0600

    added personal site

---
+++ personal_site/script.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+
+  $("button").click(function(){
+    $(".header-circle").css("background-image", "url(./media/dance.gif)");
+    $("button").html("Look at him go!");
+  });
+
+});
